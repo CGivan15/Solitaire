94297db996c65016d6bf986df311962b556b0adb
chris.givan <chrisgivan502@gmail.com>
2019-03-25 15:41:29 -0400

    Add vcs.xml file

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,11 +1,21 @@
+import java.util.*
+
 class Deck {
-    val cards = Array(52, { Card(it % 13, getSuit(it)) }
-    )
+    val cards = Array(52, { Card(it % 13, getSuit(it)) })
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset() {
+        cardsInDeck = cards.toMutableList()
+        cardsInDeck.forEach{ it.faceUp = false }
+        Collections.shuffle(cardsInDeck)
+    }
 
     private fun getSuit(i: Int) = when(i/13){
-            0 -> "Clubs"
-            1 -> "Diamonds"
-            2 -> "Hearts"
-            else -> "Spades"
+            0 -> clubs
+            1 -> diamonds
+            2 -> hearts
+            else -> spades
         }
     }
